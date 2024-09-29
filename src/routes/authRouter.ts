3013419204067560fac79abe1bc56da357665a29
motofeleaemanuel <motofeleaemanuel2009@gmail.com>
2024-09-29 15:20:00 +0300

    removed useless getUsers route

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { checkForAuthentication, getUser, login, register, verifyEmail } from '../controllers/auth/authController';
+import { checkForAuthentication, login, register, verifyEmail } from '../controllers/auth/authController';
 import authMiddleware from '../middlewares/authMiddleware';
 import refreshToken from '../controllers/auth/tokenController';
 const router = express.Router();
@@ -7,7 +7,6 @@ const router = express.Router();
 router.post('/register', register)
 router.get('/verify-email', verifyEmail)
 router.post('/login', login)
-router.get("/get-user", authMiddleware, getUser)
 router.post("/refresh-token", refreshToken)
 router.post("/check", authMiddleware, checkForAuthentication)
 
